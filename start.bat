b160f92bfeeaa6cf8b25cb8b2a5837274f150406
Andrej A Antonov <polymorphm@gmail.com>
2010-11-03 15:25:45 +0300

    removed MsWindows instructions and bats

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-path %0.rel\..\lib\Python26;%PATH%
-
-start pythonw %0.rel\..\LiRuReferer
-
-
--- start_with_console.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-path %0.rel\..\lib\Python26;%PATH%
-
-python %0.rel\..\LiRuReferer
-
-pause
-
